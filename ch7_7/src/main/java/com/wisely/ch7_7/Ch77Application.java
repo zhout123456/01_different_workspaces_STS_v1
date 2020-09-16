271b96884a1ca59b4b3d34ca6823e4e9072dd9d2
ZhouT <752074541@qq.com>
2020-09-16 17:09:46 +0800

    20200916:17-09，初始化导入ch7_7初始化项目。

---
+++ ch7_7/src/main/java/com/wisely/ch7_7/Ch77Application.java
@@ -0,0 +1,13 @@
+package com.wisely.ch7_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch77Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch77Application.class, args);
+	}
+
+}
---
+++ ch7_7/src/test/java/com/wisely/ch7_7/Ch77ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisely.ch7_7;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Ch77ApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
